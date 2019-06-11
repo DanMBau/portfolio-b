585f3e70452f7e54247655761f7f998fa1d0b73d
DocHolidayDeluxe <dabauer@me.com>
2019-06-11 18:33:11 -0400

    portfolio upload

---
+++ js/main.js
@@ -0,0 +1,167 @@
+(function ($) {
+	"use strict";
+	var nav = $('nav');
+  var navHeight = nav.outerHeight();
+  
+  $('.navbar-toggler').on('click', function() {
+    if( ! $('#mainNav').hasClass('navbar-reduce')) {
+      $('#mainNav').addClass('navbar-reduce');
+    }
+  })
+
+  // Preloader
+  $(window).on('load', function () {
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function () {
+        $(this).remove();
+      });
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
+
+	/*--/ Star ScrollTop /--*/
+	$('.scrolltop-mf').on("click", function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1000);
+	});
+
+	/*--/ Star Counter /--*/
+	$('.counter').counterUp({
+		delay: 15,
+		time: 2000
+	});
+
+	/*--/ Star Scrolling nav /--*/
+	$('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: (target.offset().top - navHeight + 5)
+				}, 1000, "easeInOutExpo");
+				return false;
+			}
+		}
+	});
+
+	// Closes responsive menu when a scroll trigger link is clicked
+	$('.js-scroll').on("click", function () {
+		$('.navbar-collapse').collapse('hide');
+	});
+
+	// Activate scrollspy to add active class to navbar items on scroll
+	$('body').scrollspy({
+		target: '#mainNav',
+		offset: navHeight
+	});
+	/*--/ End Scrolling nav /--*/
+
+	/*--/ Navbar Menu Reduce /--*/
+	$(window).trigger('scroll');
+	$(window).on('scroll', function () {
+		var pixels = 50; 
+		var top = 1200;
+		if ($(window).scrollTop() > pixels) {
+			$('.navbar-expand-md').addClass('navbar-reduce');
+			$('.navbar-expand-md').removeClass('navbar-trans');
+		} else {
+			$('.navbar-expand-md').addClass('navbar-trans');
+			$('.navbar-expand-md').removeClass('navbar-reduce');
+		}
+		if ($(window).scrollTop() > top) {
+			$('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
+		} else {
+			$('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
+		}
+	});
+
+	/*--/ Star Typed /--*/
+	if ($('.text-slider').length == 1) {
+    var typed_strings = $('.text-slider-items').text();
+		var typed = new Typed('.text-slider', {
+			strings: typed_strings.split(','),
+			typeSpeed: 80,
+			loop: true,
+			backDelay: 1100,
+			backSpeed: 30
+		});
+	}
+
+	/*--/ Testimonials owl /--*/
+	$('#testimonial-mf').owlCarousel({
+		margin: 20,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1,
+			}
+		}
+	});
+
+})(jQuery);
+
+
+var config = {
+	apiKey: "AIzaSyAtc2fwaxLRiWd0C0UpiU9nS-xU6CHH-Ag",
+	authDomain: "portfolio-contact-c0a78.firebaseapp.com",
+	databaseURL: "https://portfolio-contact-c0a78.firebaseio.com",
+	projectId: "portfolio-contact-c0a78",
+	storageBucket: "portfolio-contact-c0a78.appspot.com",
+	messagingSenderId: "817796728218"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#submit").on("click", function (event) {
+	event.preventDefault();
+
+	// Grabs user input
+	var name = $("#name").val().trim();
+	var email = $("#email").val().trim();
+	var subject = $("#subject").val().trim();
+	var message = $("#message").val().trim();
+
+	// Creates local "temporary" object for holding employee data
+	var newContact = {
+			name: name,
+			email: email,
+			subject: subject,
+			message: message,
+
+	};
+
+	// Uploads employee data to the database
+	database.ref().push(newContact);
+
+	// Logs everything to console
+	console.log(newContact.name);
+	console.log(newContact.email);
+	console.log(newContact.subject);
+	console.log(newContact.message);
+
+	alert("Thank you for your message!");
+
+	// Clears all of the text-boxes
+	$("#name").val("");
+	$("#email").val("");
+	$("#subject").val("");
+	$("#message").val("");
+});
+
